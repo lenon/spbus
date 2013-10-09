72b9142caf6524c8680b8d954e4020938781a265
Lenon Marcel <lenon.marcel@gmail.com>
2013-10-09 00:48:38 -0300

    Remove authentication support from Request.

--- lib/spbus/request.rb
+++ lib/spbus/request.rb
@@ -1,18 +1,12 @@
 module SpBus
   class Request
 
-    AUTH_URL = "http://olhovivo.sptrans.com.br/"
-
-    attr_writer :authenticated
-
     def initialize(url)
       @url = url
-      @authenticated = true
     end
 
     def get
       tries ||= 3
-      get_cookies if @authenticated
       open(@url, request_headers).read
     rescue Errno::ETIMEDOUT => e
       raise e if (tries -= 1) == 0
@@ -21,13 +15,8 @@ module SpBus
 
     private
 
-    def get_cookies
-      @cookies = open(AUTH_URL).meta["set-cookie"]
-    end
-
     def request_headers
       headers = {}
-      headers["Cookie"] = @cookies if @authenticated
       headers
     end
   end
