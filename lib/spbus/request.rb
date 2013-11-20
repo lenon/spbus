fe13698d9e07bfae089441048b144fb1f190a74f
Lenon Marcel <lenon.marcel@gmail.com>
2013-11-20 00:38:23 -0200

    Revert "Remove authentication support from Request."
    This reverts commit 72b9142caf6524c8680b8d954e4020938781a265.
    
    Conflicts:
    	lib/spbus/request.rb

--- lib/spbus/request.rb
+++ lib/spbus/request.rb
@@ -1,12 +1,18 @@
 module SpBus
   class Request
 
+    AUTH_URL = "http://olhovivo.sptrans.com.br/"
+
+    attr_writer :authenticated
+
     def initialize(url)
       @url = url
+      @authenticated = true
     end
 
     def get
       tries ||= 3
+      get_cookies if @authenticated
       open(@url, request_headers).read
     rescue Errno::ETIMEDOUT => e
       raise e if (tries -= 1) == 0
@@ -15,9 +21,14 @@ module SpBus
 
     private
 
+    def get_cookies
+      @cookies = open(AUTH_URL).meta["set-cookie"]
+    end
+
     def request_headers
       headers = {}
       headers["User-Agent"] = "SpBus/#{SpBus::VERSION}"
+      headers["Cookie"] = @cookies if @authenticated
       headers
     end
   end
