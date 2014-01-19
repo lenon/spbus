992927e131b5941be5f59f7f44570ed3e21dc36e
Lenon Marcel <lenon.marcel@gmail.com>
2014-01-19 02:07:41 -0200

    Refactor Connection specs.

--- spec/spbus/connection_spec.rb
+++ spec/spbus/connection_spec.rb
@@ -4,21 +4,21 @@ describe SpBus::Connection do
 
   describe "#get" do
 
-    context "successful GET request" do
+    context "when GET request is successful" do
       before do
         use_cassette(:successful_authentication) do
-          subject.post("Login/Autenticar", :token => sptrans_token)
+          subject.post("Login/Autenticar", :token => SpecEnv.valid_api_token)
         end
       end
 
-      it "returns the response" do
+      it "returns the response object" do
         use_cassette(:successful_get_request) do
           expect(subject.get("Corredor", :foo => "bar")).to respond_to(:body)
         end
       end
     end
 
-    context "unsuccessful GET request" do
+    context "when GET request is not successful" do
       it "raises an error" do
         use_cassette(:unsuccessful_get_request) do
           expect {
@@ -27,42 +27,59 @@ describe SpBus::Connection do
         end
       end
     end
-
-    context "cookies management" do
-      before do
-        use_cassette(:successful_authentication) do
-          subject.post("Login/Autenticar", :token => sptrans_token)
-        end
-      end
-
-      it "store cookies between requests" do
-        use_cassette(:successful_get_request) do
-          expect {
-            subject.get("Corredor", :foo => "bar")
-          }.to_not raise_error
-        end
-      end
-    end
   end
 
   describe "#post" do
-
-    context "successful POST request" do
-      it "returns the response" do
+    context "when POST request is successful" do
+      it "returns the response object" do
         use_cassette(:successful_authentication) do
           expect(
-            subject.post("Login/Autenticar", :token => sptrans_token)
+            subject.post("Login/Autenticar", :token => SpecEnv.valid_api_token)
           ).to respond_to(:body)
         end
       end
     end
 
-    context "unsuccessful POST request" do
+    context "when POST request is not successful" do
       it "raises an error" do
         use_cassette(:unsuccessful_post_request) do
           expect {
             subject.post("Banana")
           }.to raise_error(SpBus::HTTPError)
+        end
+      end
+    end
+  end
+
+  describe "cookies management" do
+
+    context "first request sent" do
+      it "does not send cookies" do
+        options = hash_including(:headers => hash_excluding("Cookie" => anything))
+
+        expect(HTTParty).to receive(:post).with(anything, options).and_call_original
+
+        use_cassette(:successful_authentication) do
+          subject.post("Login/Autenticar", :token => SpecEnv.valid_api_token)
+        end
+      end
+    end
+
+    context "last response came with cookies" do
+      before do
+        use_cassette(:successful_authentication) do
+          subject.post("Login/Autenticar", :token => SpecEnv.valid_api_token)
+        end
+      end
+
+      it "store cookies to send with the next request" do
+        cookie = regexp_matcher(/^apiCredentials=[0-9a-z]{416}$/i)
+        options = hash_including(:headers => hash_including("Cookie" => cookie))
+
+        expect(HTTParty).to receive(:get).with(anything, options).and_call_original
+
+        use_cassette(:successful_get_request) do
+          subject.get("Corredor", :foo => "bar")
         end
       end
     end
